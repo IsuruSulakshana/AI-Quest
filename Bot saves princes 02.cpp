f7adc0421fcdd53f1a7e28ff2e0b67f6db170355
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-04-17 01:14:38 +0530

    start bot saves princes 02

---
+++ Bot saves princes 02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void nextMove(int n, int r, int c, vector <string> grid){
+    //your logic here
+}
+
+int main(void) {
+
+    int n, r, c;
+    vector <string> grid;
+
+    cin >> n;
+    cin >> r;
+    cin >> c;
+
+    for(int i=0; i<n; i++) {
+        string s; cin >> s;
+        grid.push_back(s);
+    }
+
+    nextMove(n, r, c, grid);
+    return 0;
+}
