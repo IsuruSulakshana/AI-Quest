1b330f78e7fc1685f2610088a2b5370490d60844
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-04-17 01:08:39 +0530

    Done! bot saves princes

--- test.cpp
+++ test.cpp
@@ -3,18 +3,64 @@
 using namespace std;
 
 void displayPathtoPrincess(int n, vector <string> grid){
-    for (int i = 0; i < grid.size(); i++)
+    int arr[4] = {0,0,0,0};
+    string c;
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < sizeof(i); j++)
+        for (int j = 0; j < n; j++)
         {
-            cout<<i<<endl;
+            c = grid[i][j];
+            
+            if (c == "m")
+            {
+                arr[0] = i;
+                arr[1] = j;
+            }
+            else if (c == "p")
+            {
+                arr[2] = i;
+                arr[3] = j;
+            }
+            else
+            {
+                continue;
+            }
         }
-        
     }
-    
+    int rangeY = arr[2]-arr[0];
+    int rangeX = arr[3]-arr[1];
+
+    if (rangeY > 0)
+    {
+        for (int i = 0; i < rangeY; i++)
+        {
+            cout<<"DOWN\n";
+        }
+    }
+    else if (rangeY < 0)
+    {
+        for (int i = 0; i < abs(rangeY); i++)
+        {
+            cout<<"UP\n";
+        }
+    }
+    if (rangeX > 0)
+    {
+        for (int i = 0; i < rangeX; i++)
+        {
+            cout<<"RIGHT\n";
+        }
+    }
+    else if (rangeX < 0)
+    {
+        for (int i = 0; i < abs(rangeX); i++)
+        {
+            cout<<"LEFT\n";
+        }
+    }
 }
 
-int main() {
+int main(void) {
 
     int m;
     vector <string> grid;
