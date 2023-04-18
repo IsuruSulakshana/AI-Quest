22ed5d063574165b783c6691b7094e07197761ea
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-04-18 23:17:40 +0530

    updated task 02

--- Bot saves princess 02.cpp
+++ Bot saves princess 02.cpp
@@ -2,21 +2,71 @@
 #include <vector>
 using namespace std;
 
-void nextMove(int n, int r, int c, vector <string> grid){
-    //your logic here
+void nextMove(int n, int r, int c, vector<string> grid)
+{
+    int arr[4] = {r, c, 0, 0};
+    string s;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            s = grid[i][j];
+            if (s == "p")
+            {
+                arr[2] = i;
+                arr[3] = j;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    int rangeY = arr[2] - arr[0];
+    int rangeX = arr[3] - arr[1];
+
+    if (rangeX > rangeY)
+    {
+        if (rangeY > 0)
+        {
+
+            cout << "DOWN\n";
+        }
+        else if (rangeY < 0)
+        {
+
+            cout << "UP\n";
+        }
+    }
+    else if (rangeY >= rangeX)
+    {
+        if (rangeX > 0)
+        {
+
+            cout << "RIGHT\n";
+        }
+        else if (rangeX < 0)
+        {
+
+            cout << "LEFT\n";
+        }
+    }
 }
 
-int main(void) {
+int main(void)
+{
 
     int n, r, c;
-    vector <string> grid;
+    vector<string> grid;
 
     cin >> n;
     cin >> r;
     cin >> c;
 
-    for(int i=0; i<n; i++) {
-        string s; cin >> s;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
         grid.push_back(s);
     }
 
